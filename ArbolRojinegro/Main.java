9951a5c57bc6e091ed092714027e895381256b57
Antonella Badami <cantobadami@gmail.com>
2023-10-26 08:53:36 -0300

    practico

---
+++ ArbolRojinegro/Main.java
@@ -0,0 +1,11 @@
+package ArbolRojinegro;
+
+import ArbolRojinegro.Utils.Arbol;
+
+public class Main {
+    public static void main(String[] args) {
+        Arbol arbol = new Arbol();
+
+        arbol.insertar(20);
+    }
+}
