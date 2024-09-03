c7d3b76f7e7f5b8059200d6495ea8d8d5b7af553
Antonella Badami <cantobadami@gmail.com>
2024-09-03 18:30:07 -0300

    .

--- Recursividad/Practica.java
+++ Recursividad/Practica.java
@@ -35,8 +35,42 @@ public class Practica {
             recursiva(n-1);
         }
     }
+
+    public static int[] ordenar(int array[], int n){
+
+        if( n == 0){
+            return array;
+        }else{
+            int indexMax = 0;
+            for(int i = 1; i < n+1 ; i++){
+                if(array[i] > array[indexMax]){
+                    indexMax = i;
+                }  
+            }
+            int temp = array[n];
+            array[n] = array[indexMax];
+            array[indexMax] = temp;
+            return ordenar(array, n-1);
+        }
+    }
+
+    public static int calcularMaximo(int array[], int max, int n) throws Exception{
+        if(array.length == 0){
+            throw new Exception("arreglo vacio");
+        }
+        if(n == 0){
+            return max;
+        }else{
+            if(max < array[n]){
+                max = array [n];
+            }
+            
+            return calcularMaximo(array, max, n-1);
+        }
+       
+    }
     public static void main(String[] args) {
-        char array[] = {'h','o','l','a'};
+        /*char array[] = {'h','o','l','a'};
         int i = array.length;
         int v[] = {1,3,1};
         int n = v.length-1;
@@ -45,6 +79,20 @@ public class Practica {
         recursiva(c);
         System.out.println("suma: "+suma(v, n));
         System.out.println("cantidad vocales");
-        System.out.println(cantVocales(array, i));
+        System.out.println(cantVocales(array, i));*/
+
+        int array[] = {5,2,13,8,11,9,3,1};
+        int n = array.length-1;
+        int max = array[n];
+        //array = ordenar(array, n);
+       /*  for(int h = 0; h <=n; h++){
+            System.out.println(array[h] + " ");
+        }*/
+        try {
+            System.out.println(calcularMaximo(array, max, n));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
